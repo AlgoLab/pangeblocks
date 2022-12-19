05b78624810b3eb21a0ca67c6c5351fc0582bde0
Jorge Avila <jorgeavilacartes@gmail.com>
2022-12-19 16:41:30 +0100

    add time to optimization step. pipeline to download and run pandora/make_prg

--- compute_gfa.py
+++ compute_gfa.py
@@ -1,13 +1,15 @@
 """
 Compute a Variation Graph from an MSA
 """
-
+from time import time
 import json
 import argparse
 from src.blocks import Block
 from src.ilp.input import InputBlockSet
 from src.ilp.optimization import Optimization
 from src.ilp.variaton_graph_parser import asGFA
+
+from pathlib import Path
 
 # Command line options
 parser = argparse.ArgumentParser()
@@ -24,11 +26,28 @@ path_gfa = args.path_gfa
 with open(path_blocks) as fp:
     blocks = [Block(*block) for block in json.load(fp)] 
 
+ti = time()
 inputset_gen = InputBlockSet()
 inputset = inputset_gen(path_msa, blocks)
+tf = time()
+t_input = tf - ti
 
+ti = time()
 opt = Optimization(blocks=inputset, path_msa=path_msa)
 opt_coverage = opt()
+tf = time()
+t_ilp = tf - ti
 
+ti = time()
 parser=asGFA()
 parser(opt_coverage,path_gfa)
+tf = time()
+t_gfa = tf - ti
+
+path_time = Path(path_gfa).parent
+path_time.mkdir(exist_ok=True, parents=True)
+name_msa = Path(path_msa).stem
+with open(path_time.joinpath(name_msa + ".txt"), "w") as fp:
+    fp.write(f"time_input: {t_input}\n")
+    fp.write(f"time_ilp: {t_ilp}\n")
+    fp.write(f"time_gfa: {t_gfa}\n")
---
+++ make_prg.smk
@@ -0,0 +1,37 @@
+configfile: "params.yaml"
+from pathlib import Path
+
+PATH_OUTPUT = config["PATH_OUTPUT"]
+PATH_MSAS   = config["PATH_MSAS"]
+
+NAMES = [path.stem for path in Path(PATH_MSAS).rglob("*.fa")]
+
+rule all:
+    input:
+        expand("{path_output}/output-pandora/{name_msa}.gfa", name_msa=NAMES, path_output=PATH_OUTPUT)
+
+rule download_make_prg:
+    output:
+        "make_prg_0.4.0"
+    shell:
+        """
+        wget https://github.com/iqbal-lab-org/make_prg/releases/download/0.4.0/make_prg_0.4.0
+        chmod +x make_prg_0.4.0
+        """
+
+rule generate_prg:
+    input:
+        "make_prg_0.4.0"
+    output:
+        f"{PATH_OUTPUT}/output-pandora/.prg.gfa.zip"
+    shell:
+        f"./make_prg_0.4.0 from_msa -i {PATH_MSAS} -o {PATH_OUTPUT}/output-pandora/ --output-type g"
+
+rule extract_gfa: 
+    input:
+        path_zip=f"{PATH_OUTPUT}/output-pandora/.prg.gfa.zip",
+        path_output=PATH_OUTPUT
+    output: 
+        expand("{path_output}/output-pandora/{name_msa}.gfa", name_msa=NAMES,path_output=PATH_OUTPUT)
+    shell:
+        "unzip {input.path_zip} -d {input.path_output}/output-pandora"
