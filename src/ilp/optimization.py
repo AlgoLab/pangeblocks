bdb75b12170e26e1ea2a4d50c236460d9fe19ee6
Gianluca Della Vedova <gianluca@dellavedova.org>
2023-01-02 18:17:17 +0100

    dummy blocks

--- src/ilp/optimization.py
+++ src/ilp/optimization.py
@@ -85,6 +85,13 @@ class Optimization:
         times = dict()
         ti = time.time()
 
+        # all blocks we receive involve at least two sequences, but we need to
+        # add dummy blocks for the cell of the MSA.
+        # This allows to guarantee that the ILP always has a feasible solution
+        # TODO: replace * with the correct character
+        dummy_blocks = [Block((r,), c, c, '*') for r in range(self.n_seqs)
+                        for c in range(self.n_cols)]
+        all_blocks = self.input_blocks + dummy_blocks
         # covering_by_position is a dictionary with key (r,c) and value the list
         # of indices of the blocks that include the position (r,c)
         #
