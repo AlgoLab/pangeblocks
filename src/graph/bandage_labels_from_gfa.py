660695128e248423cde7e1137d90b400e60ac45e
Jorge Avila <jorgeavilacartes@gmail.com>
2023-01-03 23:07:40 +0100

    bandage labels: include sequences using a node

--- src/graph/bandage_labels_from_gfa.py
+++ src/graph/bandage_labels_from_gfa.py
@@ -1,8 +1,6 @@
 import argparse
 from pathlib import Path
-from collections import namedtuple
-
-
+from collections import namedtuple, defaultdict
 
 def bandage_labels(path_gfa: str, path_save_labels= str):
     """
@@ -12,7 +10,8 @@ def bandage_labels(path_gfa: str, path_save_labels= str):
 
     BandageLabels = namedtuple("BandageLables",["node_name","first_base","last_base", "seq"])
     info_labels = []
-    
+    seqs_by_node = defaultdict(list)
+
     with open(path_gfa) as fp:
         for line in fp:
             line=line.replace("\n","")
@@ -24,17 +23,27 @@ def bandage_labels(path_gfa: str, path_save_labels= str):
                 last_base=seq[-1]
                 info_labels.append(BandageLabels(node_name, first_base, last_base, seq))
             
-    # Write info in csv file
-    colnames = ",".join(BandageLabels._fields)+"\n"
+            if line.startswith("P"):
+                split_line = line.split("\t")
+                seq_id = split_line[1]
+                path   = split_line[-1].replace("+","")
+                nodes_path = [int(node_id) for node_id in path.split(",")]
+                for node_id in nodes_path:
+                    seqs_by_node[node_id].append(seq_id)
 
-    filename_no_extension = Path(path_gfa).stem
+
+    # Write info in csv file
+    colnames = ",".join(BandageLabels._fields)+ ",list_seqs" + "\n"
 
     path_save = Path(path_save_labels)
     path_save.parent.mkdir(exist_ok=True, parents=True)
     with open(path_save,"w") as fp:
         fp.write(colnames)
         for label in info_labels:
-            fp.write(f"{label.node_name},{label.first_base},{label.last_base},{label.seq}\n")
+            list_seqs = seqs_by_node[int(label.node_name)]
+            list_seqs.sort()
+            list_seqs = "-".join(list_seqs)
+            fp.write(f"{label.node_name},{label.first_base},{label.last_base},{label.seq},{list_seqs}\n")
 
 if __name__ == "__main__":
     parser=argparse.ArgumentParser()
